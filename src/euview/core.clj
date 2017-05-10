0762c54e512e3fc90769354844697e1c4c090602
GregorStocks <gregor@gregorstocks.com>
2017-05-10 11:53:45 -0700

    progress

--- src/euview/core.clj
+++ src/euview/core.clj
@@ -1,6 +1,8 @@
 (ns euview.core
   (:gen-class)
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [euview.savegame :as savegame]
+            [euview.render :as render]))
 
 (defn zip-contents->eu4-file [c]
   ;; wat
@@ -15,10 +17,11 @@
     (zip-contents->eu4-file z)))
 
 (defn france []
-  (resource->eu4-file (io/resource "France1449_01_25.eu4")))
+  (savegame/parse-savegame (resource->eu4-file (io/resource "France1449_01_25.eu4"))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (let [f (france)]
-    ))
+    (render/render-gif france "france.gif")
+    (println "Rendered france.gif")))
--- src/euview/render.clj
+++ src/euview/render.clj
@@ -1,8 +1,9 @@
 (ns euview.render
-  (:import java.awt.Image
-           java.awt.image.BufferedImage)
-  (:require [clojure.java.io :as io]
-            [gif-clj.core :as gif]))
+  (:import [java.awt Image Color Font]
+           java.awt.image.BufferedImage
+           com.github.gif.AnimatedGifEncoder)
+  
+  (:require [clojure.java.io :as io]))
 
 (defn load-map []
   (let [scale-factor 4
@@ -21,15 +22,56 @@
     (slurp (io/resource "definition.csv"))
     {}))
 
-(defn province-owner-at [year])
+(defn province-owner-at [year]
+  "FRA")
 
-(defn year->frame [savegame map provinces year]
-  (println "constructing frame" year)
-  map)
+(def ocean-color Color/BLUE)
+(defn draw-oceans [frame]
+  (doto (.createGraphics frame)
+    (.setColor ocean-color)
+    (.fillRect 0 0 (.getWidth frame) (.getHeight frame))))
+
+(def transparent-color (Color. 69 69 69 69))
+(defn draw-transparent [frame]
+  (doto (.createGraphics frame)
+    (.setColor transparent-color)
+    (.fillRect 0 0 (.getWidth frame) (.getHeight frame))))
+
+(defn write-year [frame year]
+  (doto (.createGraphics frame)
+    (.setColor ocean-color)
+    (.fillRect 0 00 100 100)
+    (.setColor Color/BLACK)
+    (.setFont (Font. "SansSerif" Font/BOLD 12))
+    (.drawString (str year) 50 50)))
+
+(defn year->frame [savegame
+                   ^BufferedImage map
+                   provinces
+                   year]
+  (let [frame (BufferedImage. (.getWidth map) (.getHeight map) (.getType map))]
+    (draw-transparent frame)
+    (println "constructing frame" year)
+    (write-year frame year)
+    frame))
+
+(defn initial-frame [map savegame]
+  (let [frame (BufferedImage. (.getWidth map) (.getHeight map) (.getType map))]
+    (draw-oceans frame)
+    (write-year frame "1444")
+    frame))
 
 (defn frames->gif [map frames gif-filename]
-  (with-open [w (io/output-stream gif-filename)]
-    (.write w (gif/build-animated-gif 10 0 (cons map frames)))))
+  (let [encoder (AnimatedGifEncoder.)]
+    (doto encoder 
+      (.start gif-filename)
+      (.setDelay 10)
+      (.setRepeat 0)
+      (.setDispose 1)
+      (.setTransparent transparent-color))
+    (doseq [frame frames]
+      (.addFrame encoder frame))
+    (.finish encoder)))
 
 (defn construct-provinces [savegame map-bmp pid->color]
   (for [[k v] (-> savegame :variables (get "provinces"))]
@@ -40,7 +82,7 @@
 
 (defn render-gif [parsed-savegame gif-filename]
   (let [map (load-map)
-        provinces (construct-provinces parsed-savegame map (load-provinces-csv))]
-    (frames->gif map
-                 (for [year (range 1444 1460)] (year->frame parsed-savegame map provinces year))
-                 gif-filename)))
+        provinces (construct-provinces parsed-savegame map (load-provinces-csv))
+        frames (cons (initial-frame map parsed-savegame)
+                     (for [year (range 1444 1460)] (year->frame parsed-savegame map provinces year)))]
+    (frames->gif map frames gif-filename)))
