6d16b2b8a91e46c3969cbfc216dd62c77604873b
GregorStocks <gregor@gregorstocks.com>
2017-05-13 14:52:03 -0700

    1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject euview "0.1.0-SNAPSHOT"
+(defproject euview "1.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
